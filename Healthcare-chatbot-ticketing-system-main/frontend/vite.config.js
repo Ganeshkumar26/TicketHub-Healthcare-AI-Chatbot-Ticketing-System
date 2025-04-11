795d91fddd638260dd65f7401e74a383bb44b4a3
Ganesh Kumar <144578914+Ganeshkumar26@users.noreply.github.com>
2025-04-11 11:44:22 +0530

    Update vite.config.js

--- Healthcare-chatbot-ticketing-system-main/frontend/vite.config.js
+++ Healthcare-chatbot-ticketing-system-main/frontend/vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'TicketHub-Healthcare-AI-Chatbot-Ticketing-System',
+  ...
+})
+
+export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
